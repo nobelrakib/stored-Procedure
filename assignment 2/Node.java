aadf2f9c97782daf048d692bf71243ec8a41a4cb
Rakib Hossain <nobelrakib03@gmail.com>
2019-10-23 16:15:42 +0600

    added

---
+++ assignment 2/Node.java
@@ -0,0 +1,10 @@
+public class Node{
+    int key;
+    Node next;
+    
+    public Node(int v, Node n){
+        key = v;
+        next = n;
+    }
+    
+}
---
+++ assignment 2/tester33.java
@@ -0,0 +1,5 @@
+class tester33{
+  public static void main(String []args){
+    MyOrderList list=new MyOrderList();
+  }
+}
