a4b3a6d4878118057120aa5d913302313b8375eb
Rakib Hossain <nobelrakib03@gmail.com>
2019-10-25 14:53:13 +0600

    practicefortest

---
+++ practicefortest/WebApi.Core/Model/StudentBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApi.Core
+{
+    public class StudentBook
+    {
+        public int StudentBookId { get; set; }
+        public int StudentId { get; set;}
+        public string bookbarcode { get; set;}
+        public Book Book { get; set;}
+        public Student Student { get; set;}
+        public DateTime IssueDate { get; set;}
+        public DateTime ReturneDate { get; set;}
+    }
+}
---
+++ practicefortest/WebApi.Store/LibraryContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Core;
+namespace WebApi.Store
+{
+    public class LibraryContext : DbContext
+    {
+        private string _connectionString;
+        private string _migrationAssemblyName;
+        public LibraryContext(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<StudentBook>()
+            .HasKey(sb => new { sb.StudentId, sb.bookbarcode });
+            builder.Entity<StudentBook>()
+                .HasOne(sb => sb.Student)
+                .WithMany(sb => sb.Books)
+                .HasForeignKey(sb => sb.StudentId);
+            base.OnModelCreating(builder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            if (!optionBuilder.IsConfigured)
+            {
+                optionBuilder.UseSqlServer(_connectionString, m => m.MigrationsAssembly(_migrationAssemblyName));
+            }
+            base.OnConfiguring(optionBuilder);
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<StudentBook> StudentBooks { get; set; }
+    }
+}
